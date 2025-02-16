46dc005bb319757eb3097f7f3d2f24bd2f5ae276
Владислав <foo442866@gmail.com>
2025-02-16 21:13:00 +0300

    add class food

--- Lab1_sem2/NewYear/models/guest.py
+++ Lab1_sem2/NewYear/models/guest.py
@@ -1,8 +1,9 @@
+from gift import Gift
+
 class Guest:
-    def __init__(self, name, age, status, gifts, active):
+    def __init__(self, name, age, status, gifts: Gift, active):
         self.__name = name
         self.__age = age
         self.__status = status
         self.__gifts = []
         self.__active = False
-

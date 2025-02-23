51cae63998b3a7fd01409f3463c732f9eca7336d
Владислав <foo442866@gmail.com>
2025-02-23 18:02:54 +0300

    отредактировал класс декорации для соответствия PEP8: анотация типов, __repr__, имена, добавил пару проверок

--- Lab1_sem2/NewYear/models/decoration.py
+++ Lab1_sem2/NewYear/models/decoration.py
@@ -1,24 +1,37 @@
 class Decoration:
-    def __init__(self, type, place, color):
-        self.__type = type
-        self.__place = place
-        self.__color = color
+    def __init__(self, decor_type: str, place: str, color: str):
+        if not isinstance(decor_type, str):
+            raise TypeError("Тип украшения должен быть строкой.")
+        if not isinstance(place, str):
+            raise TypeError("Место должно быть строкой.")
+        if not isinstance(color, str):
+            raise TypeError("Цвет должен быть строкой.")
+
+        self._type = decor_type
+        self._place = place
+        self._color = color
 
     @property
-    def type(self):
-        return self.__type
+    def type(self) -> str:
+        return self._type
 
     @property
-    def color(self):
-        return self.__color
+    def color(self) -> str:
+        return self._color
 
     @property
-    def place(self):
-        return self.__place
+    def place(self) -> str:
+        return self._place
 
     @place.setter
-    def place(self, place):
-        self.__place = place
+    def place(self, new_place: str) -> None:
+        if not isinstance(new_place, str):
+            raise TypeError("Место должно быть строкой.")
+        self._place = new_place
 
-    def remove(self):
-        self.place = None
+    def remove(self) -> None:
+        """Удаляет украшение, сбрасывая его место."""
+        self._place = None
+
+    def __repr__(self) -> str:
+        return f"Decoration(type='{self._type}', place='{self._place}', color='{self._color}')"

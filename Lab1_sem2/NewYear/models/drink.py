06a2b79955ffc11d9d899ca55336b38655ae82b2
Владислав <foo442866@gmail.com>
2025-02-23 18:07:07 +0300

    отредактировал класс напитки для соответствия PEP8: анотация типов, __repr__, имена, добавил пару проверок

--- Lab1_sem2/NewYear/models/drink.py
+++ Lab1_sem2/NewYear/models/drink.py
@@ -2,22 +2,22 @@ from meal import Meal
 
 class Drink(Meal):
     def __init__(self, name: str, alcohol: bool, temperature: str):
-
         if not isinstance(alcohol, bool):
-            raise TypeError("Поле 'alcohol' должно быть True или False.")
+            raise TypeError("Поле '_alcohol' должно быть True или False.")
         if temperature not in ("холодный", "горячий"):
             raise ValueError("Температура должна быть 'холодный' или 'горячий'.")
 
         super().__init__(name)
-        self.__temperature = temperature
-        self.__alcohol = alcohol
+        self._temperature = temperature
+        self._alcohol = alcohol
 
-        @property
-        def temperature(self):
-            return self.__temperature
+    @property
+    def temperature(self) -> str:
+        return self._temperature
 
-        @property
-        def alcohol(self):
-            return self.__alcohol
+    @property
+    def alcohol(self) -> bool:
+        return self._alcohol
 
-
+    def __repr__(self) -> str:
+        return f"Drink(name='{self.name}', alcohol={self._alcohol}, temperature='{self._temperature}')"

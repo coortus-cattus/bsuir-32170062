ebbf2d9dfdab7f0a91637fb67fc7c94cb0c23991
Владислав <foo442866@gmail.com>
2025-02-23 18:14:14 +0300

    отредактировал класс развелечения для соответствия PEP8: анотация типов, __repr__, имена, добавил пару проверок

--- Lab1_sem2/NewYear/models/entertainment.py
+++ Lab1_sem2/NewYear/models/entertainment.py
@@ -1,52 +1,56 @@
+from typing import List
+
 from guest import Guest
 
+
 class Entertainment:
-    def __init__(self, name: str, host: Guest, description: str, activeGuests: list[Guest] = None):
+    def __init__(self, name: str, host: Guest, description: str, active_guests: List[Guest] = None):
         if not isinstance(name, str):
             raise TypeError("Название должно быть строкой.")
         if not isinstance(host, Guest):
-            raise TypeError("Ведущий должен быть в списке гостей.")
+            raise TypeError("Ведущий должен быть объектом Guest.")
         if not isinstance(description, str):
             raise TypeError("Описание должно быть строкой.")
-        if activeGuests is not None and not isinstance(activeGuests, list) or not all(isinstance(a, Guest) for a in activeGuests):
-            raise TypeError("ActiveGuests должен быть списком элементов типа Guest")
+        if active_guests is not None and (not isinstance(active_guests, list) or not all(isinstance(a, Guest) for a in active_guests)):
+            raise TypeError("active_guests должен быть списком элементов типа Guest.")
 
-        self.__name = name
-        self.__host = host
-        self.__description = description
-        self.__activeGuests = activeGuests if activeGuests is not None else []
-        self.__status = "Не начато."
-
-    @property
-    def name(self):
-        return self.__name
+        self._name = name
+        self._host = host
+        self._description = description
+        self._active_guests = active_guests if active_guests is not None else []
+        self._status = "Не начато."
 
     @property
-    def host(self):
-        return self.__host
+    def name(self) -> str:
+        return self._name
 
     @property
-    def description(self):
-        return self.__description
+    def host(self) -> Guest:
+        return self._host
 
     @property
-    def activeGuests(self):
-        return self.__activeGuests
+    def description(self) -> str:
+        return self._description
 
     @property
-    def status(self):
-        return self.__status
+    def active_guests(self) -> List[Guest]:
+        return self._active_guests
 
-    def startAct(self):
-        self.__status = f"Развлечение {self.__name} началось."
+    @property
+    def status(self) -> str:
+        return self._status
 
-    def addGuest(self, guest):
-        if guest is not Guest:
-            return "Гость должен быть гостем."
-        self.__activeGuests.append(guest)
+    def start_act(self) -> None:
+        self._status = f"Развлечение '{self._name}' началось."
 
-    def finishAct(self):
-        self.__status = f"Развлечение {self.__name} окончилось."
+    def add_guest(self, guest: Guest) -> str:
+        if not isinstance(guest, Guest):
+            return "Гость должен быть объектом Guest."
+        self._active_guests.append(guest)
+        return f"Гость {guest} добавлен в список активных участников."
 
+    def finish_act(self) -> None:
+        self._status = f"Развлечение '{self._name}' окончено."
 
-
+    def __repr__(self) -> str:
+        return f"Entertainment(name='{self._name}', host={self._host}, status='{self._status}')"
